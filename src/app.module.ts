517a38fc1a787a90cc406f51b5f5471a06eabe41
Suliko <79033456672sd@gmail.com>
2021-02-27 00:22:45 +0300

    - Переменные среды

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+const login = process.env.MONGO_LOGIN;
+const password = process.env.MONGO_PASSWORD;
+
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:qzWMj7ENb63ToBAG@cluster0.x1dek.mongodb.net/test?retryWrites=true&w=majority',
+      `mongodb+srv://${login}:${password}@cluster0.x1dek.mongodb.net/test?retryWrites=true&w=majority`,
       { useNewUrlParser: true },
     ),
     CatsModule,
