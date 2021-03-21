f93b88867567e484f4f858a4e8a4dd1700107807
Suliko <79033456672sd@gmail.com>
2021-03-21 21:13:54 +0300

    - Добавлен Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { CitiesModule } from './cities/cities.module';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { CatsModule } from './cats/cats.module';
-//
-// const MONGO_LOGIN = process.env.MONGO_LOGIN;
-// const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+// import { GraphQLModule } from '@nestjs/graphql';
+// import { CitiesModule } from './cities/cities.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
+
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_DB,
+  MONGO_PORT,
+} = process.env;
+
+const uri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+console.log('=======================');
+console.log(uri);
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+};
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      installSubscriptionHandlers: true,
-      autoSchemaFile: './schema.gql',
-    }),
-    CitiesModule,
-    // MongooseModule.forRoot(
-    //   `mongodb+srv://${MONGO_LOGIN}:${MONGO_PASSWORD}@cluster0.x1dek.mongodb.net/test?retryWrites=true&w=majority`,
-    //   { useNewUrlParser: true },
-    // ),
-    // CatsModule,
+    // GraphQLModule.forRoot({
+    //   installSubscriptionHandlers: true,
+    //   autoSchemaFile: './schema.gql',
+    // }),
+    // CitiesModule,
+    MongooseModule.forRoot(uri, options),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
